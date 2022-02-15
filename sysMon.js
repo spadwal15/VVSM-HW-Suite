66d9c9b87164c3ed95e024074c4779c31b4fef83
Mayur pawar <76616775+Mayurpawar1611@users.noreply.github.com>
2022-02-15 19:05:06 +0530

    Added Cpu core and hdd temps

--- sysMon.js
+++ sysMon.js
@@ -9,99 +9,142 @@ const sysMon = () => {
     //OS
     ipcMain.on('os-info', e => {
         // CPU information
-        si.osInfo().then(osi=>{
-        const osInfo=[];
-        osInfo.push(osi.platform);
-        //osInfo
-        e.sender.send('os-info',osInfo);
-        //console.log(cpuInfo);
+        si.osInfo().then(osi => {
+            const osInfo = [];
+            osInfo.push(osi.platform);
+            //osInfo
+            e.sender.send('os-info', osInfo);
+            //console.log(cpuInfo);
         }).catch(error => console.error(error));
     });
 
     //CPU
     ipcMain.on('cpu-info', e => {
         // CPU information
-        si.cpu().then(cpu=>{
-        const cpuInfo=[];
-        cpuInfo.push(cpu.manufacturer,cpu.brand,cpu.cores);
-        //console.log(cpu['manufacturer']);
-        //cpuInfo.push(cpu['brand']);
-        //cpuInfo.push(cpu['cores']);
-        e.sender.send('cpu-info',cpuInfo);
-        //console.log(cpuInfo);
+        si.cpu().then(cpu => {
+            const cpuInfo = [];
+            cpuInfo.push(cpu.manufacturer, cpu.brand, cpu.cores);
+            //console.log(cpu['manufacturer']);
+            //cpuInfo.push(cpu['brand']);
+            //cpuInfo.push(cpu['cores']);
+            e.sender.send('cpu-info', cpuInfo);
+            //console.log(cpuInfo);
         }).catch(error => console.error(error));
     });
 
     ipcMain.on('cpu-temp-info', e => {
         // CPU information
-        si.cpuTemperature().then(cpuTemperature=>{
-        const cpuTemp=[];
-        cpuTemp.push(cpuTemperature.main,cpuTemperature.max, cpuTemperature.cores);
-        //console.log(cpu['temp main']);
-        //cpuInfo.push(cpu['temp max']);
-        // console.log("test2");
-        e.sender.send('cpu-temp-info',cpuTemp);
-        // console.log("test3");
-        //console.log(cpuInfo);
+        si.cpuTemperature().then(cpuTemperature => {
+            const cpuTemp = [];
+            cpuTemp.push(cpuTemperature.main, cpuTemperature.max, cpuTemperature.cores);
+            //console.log(cpu['temp main']);
+            //cpuInfo.push(cpu['temp max']);
+            // console.log("test2");
+            e.sender.send('cpu-temp-info', cpuTemp);
+            // console.log("test3");
+            //console.log(cpuInfo);
         }).catch(error => console.error(error));
     });
 
     //GPU
     ipcMain.on('gpu-info', e => {
         // GPU information
-        si.graphics().then(graphics=>{
-        const gpu=[];
-        gpu.push(graphics.controllers[0].model,graphics.controllers[0].vram/1024,graphics.controllers[0].vramDynamic,graphics.displays[0].vendor,graphics.displays[0].model,graphics.displays[0].resolutionX,graphics.displays[0].resolutionY);
+        si.graphics().then(graphics => {
+            const gpu = [];
+            gpu.push(graphics.controllers[0].model, graphics.controllers[0].vram / 1024, graphics.controllers[0].vramDynamic, graphics.displays[0].vendor, graphics.displays[0].model, graphics.displays[0].resolutionX, graphics.displays[0].resolutionY);
 
-        //gpuInfo.push(gpu['model']);
-        //gpuInfo.push(gpu['vram']);
-        //gpuInfo.push(gpu['cores']);
+            //gpuInfo.push(gpu['model']);
+            //gpuInfo.push(gpu['vram']);
+            //gpuInfo.push(gpu['cores']);
 
-        //gpuInfo.push(display['vendor']);
-        //gpuInfo.push(display['model']);
-        //gpuInfo.push(display['resolution']);
-        e.sender.send('gpu-info',gpu);
-        //console.log(gpuInfo);
+            //gpuInfo.push(display['vendor']);
+            //gpuInfo.push(display['model']);
+            //gpuInfo.push(display['resolution']);
+            e.sender.send('gpu-info', gpu);
+            //console.log(gpuInfo);
         }).catch(error => console.error(error));
     });
 
     ipcMain.on('com', e => {
 
         // PC information
-        let Uptime = os.uptime()/60;
+        let Uptime = os.uptime() / 60;
 
         let OsType = os.type()
 
-        const com=[];
+        const com = [];
         com.push(Uptime, OsType)
 
-        e.sender.send('com',com);
+        e.sender.send('com', com);
 
     });
 
     ipcMain.on('memory', e => {
         // CPU information
-        si.mem().then(mem=>{
-        const memory=[];
-        total = mem.total/1073741824;
-        let FreeMemory = os.freemem()/1073741824;
-        memory.push(total, FreeMemory);
-        //osInfo
-        e.sender.send('memory',memory);
-        //console.log(cpuInfo);
+        si.mem().then(mem => {
+            const memory = [];
+            total = mem.total / 1073741824;
+            let FreeMemory = os.freemem() / 1073741824;
+            memory.push(total, FreeMemory);
+            //osInfo
+            e.sender.send('memory', memory);
+            //console.log(cpuInfo);
         }).catch(error => console.error(error));
     });
 
-    ipcMain.on('temps', e => {
+    ipcMain.on('cputemps', e => {
         // Temps
-        fs.readFile('Logs/Temps.txt', 'utf8' , (err, data) => {
-          if (err) {
-            console.error(err)
-            return
-          }
-          e.sender.send('temps',data);
+        fs.readFile('Logs/Temps.txt', encoding = 'utf8', (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            
+            let temps = data.toString().split("\n");
+            let cpuStr='CPU Core';
+            let cpuArray=[]; 
+            for (i in temps) {
+               if(temps[i].match(cpuStr)){
+                let tempSlice=temps[i].slice(-16)
+                cpuArray.push(tempSlice);
+               }
+            }
+            e.sender.send('cputemps', cpuArray);
+
+            console.log(cpuArray);
+           
+           
+        })
+    });
+    
+    ipcMain.on('hddtemps', e => {
+        // Temps
+        fs.readFile('Logs/Temps.txt', encoding = 'utf8', (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            
+            let temps = data.toString().split("\n");
+            let hddStr='HDD';
+            let hddArray=[];
+            for (i in temps) {
+               if(temps[i].match(hddStr)){
+                
+                let hddSlice=temps[i].slice(0,24)+' '+temps[i].slice(-19);
+                
+                hddArray.push(hddSlice);
+              
+
+               }
+            }
+            e.sender.send('hddtemps', hddArray);
+
+            console.log(hddArray);
+           
+           
         })
     });
 }
 
-module.exports=sysMon;
+module.exports = sysMon;
--- sysMon.py
+++ sysMon.py
@@ -1,14 +1,20 @@
 import clr
 
 
-openhardwaremonitor_hwtypes = ['Mainboard','SuperIO','CPU','RAM','GpuNvidia','GpuAti','TBalancer','Heatmaster','HDD']
+openhardwaremonitor_hwtypes = ['Mainboard', 'SuperIO', 'CPU',
+                               'RAM', 'GpuNvidia', 'GpuAti', 'TBalancer', 'Heatmaster', 'HDD']
 
-openhardwaremonitor_sensortypes = ['Voltage','Clock','Temperature','Load','Fan','Flow','Control','Level','Factor','Power','Data','SmallData']
+openhardwaremonitor_sensortypes = ['Voltage', 'Clock', 'Temperature', 'Load',
+                                   'Fan', 'Flow', 'Control', 'Level', 'Factor', 'Power', 'Data', 'SmallData']
 
-file = open('G:\VVSM-HW-Suite\Logs\Temps.txt','w')
 
 def initialize_openhardwaremonitor():
-    file = r'G:\VVSM-HW-Suite\DLLs\OpenHardwareMonitorLib'
+    with open('Logs\Temps.txt', 'w') as outfile:
+        outfile.truncate()
+    with open('Logs\Temps1.txt', 'w') as outfile:
+        outfile.truncate()
+
+    file = r'C:\Users\Rohit\Desktop\Mayur Files\VVSM-HW-Suite-main\DLLs\OpenHardwareMonitorLib.dll'
     clr.AddReference(file)
 
     from OpenHardwareMonitor import Hardware
@@ -21,6 +27,7 @@ def initialize_openhardwaremonitor():
     handle.HDDEnabled = True
     handle.Open()
     return handle
+
 
 def fetch_stats(handle):
     for i in handle.Hardware:
@@ -35,33 +42,46 @@ def fetch_stats(handle):
 
 def parse_sensor(sensor):
 
-        if sensor.Value is not None:
-            if type(sensor).__module__ == 'OpenHardwareMonitor.Hardware':
-                sensortypes = openhardwaremonitor_sensortypes
-                hardwaretypes = openhardwaremonitor_hwtypes
-            else:
-                return
+    if sensor.Value is not None:
+        if type(sensor).__module__ == 'OpenHardwareMonitor.Hardware':
+            sensortypes = openhardwaremonitor_sensortypes
+            hardwaretypes = openhardwaremonitor_hwtypes
+        else:
+            return
 
-            if sensor.SensorType == sensortypes.index('Temperature'):
-                print(u"%s %s Temperature Sensor #%i %s - %s\u00B0C" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
-                file.write(u"%s %s Temperature Sensor #%i %s - %s\u00B0C \n" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+        if sensor.SensorType == sensortypes.index('Temperature'):
+            with open('Logs\Temps.txt', 'a') as outfile:
+                outfile.write(u"%s %s Temperature Sensor #%i %s : %s\n" % (
+                    hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
 
-            elif sensor.SensorType == sensortypes.index('Fan'):
-                print(u"%s %s Fan Speed #%i %s - %s RPM" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+            with open('Logs\Temps1.txt', 'a') as outfile:
+                outfile.write(u"%s\n" % (sensor.Value))
 
-            elif sensor.SensorType == sensortypes.index('Voltage'):
-                print(u"%s %s Voltage #%i %s - %s V" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+            print(u"%s %s Temperature Sensor #%i %s - %s\u00B0C" %
+                  (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
 
-            elif sensor.SensorType == sensortypes.index('Clock'):
-                print(u"%s %s Clock #%i %s - %s MHz" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+        elif sensor.SensorType == sensortypes.index('Fan'):
+            print(u"%s %s Fan Speed #%i %s - %s RPM" %
+                  (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
 
-            elif sensor.SensorType == sensortypes.index('Load'):
-                print(u"%s %s Load #%i %s - %s Percent" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+        elif sensor.SensorType == sensortypes.index('Voltage'):
+            print(u"%s %s Voltage #%i %s - %s V" %
+                  (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
 
-            elif sensor.SensorType == sensortypes.index('Power'):
-                print(u"%s %s Power #%i %s - %s W" % (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+        elif sensor.SensorType == sensortypes.index('Clock'):
+            print(u"%s %s Clock #%i %s - %s MHz" %
+                  (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+
+        elif sensor.SensorType == sensortypes.index('Load'):
+            print(u"%s %s Load #%i %s - %s Percent" %
+                  (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+
+        elif sensor.SensorType == sensortypes.index('Power'):
+            print(u"%s %s Power #%i %s - %s W" %
+                  (hardwaretypes[sensor.Hardware.HardwareType], sensor.Hardware.Name, sensor.Index, sensor.Name, sensor.Value))
+
+
 if __name__ == "__main__":
     print("OpenHardwareMonitor:")
     HardwareHandle = initialize_openhardwaremonitor()
     fetch_stats(HardwareHandle)
-    file.close()
